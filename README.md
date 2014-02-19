2453d6309e45435410a3ae89edd0aa1b39e0e999
Jonathan Foote <jmfoote@loyola.edu>
2014-02-18 21:34:24 -0500

    added script, updated REAMDME to tell my story :)

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 flickrgrabr
 ===========
 
-Quick hack to get your original images back from your Flickr photostream, if you're lucky
+Quick hack to get your original images back from your Flickr photostream, if you're lucky. 
+
+# Background
+
+I found out I have an old Flickr photostream that contains some photos that I took long ago with my BlackBerry Pearl. I didn't have copies anywhere else, so I wanted to download them. 
+
+I didn't look too hard, but it seems that you can't get your originals back out of Flickr in bulk via the Flickr web UI. My searches turned up some binary apps that will download the originals for you, but being the skeptical sort that I am, I figured I would just write a Python script to do it. 
+
+## Note
+
+This script works for me, but it's brittle. I tested it out on some of the top Flickr photostreams and it didn't work. Maybe premium accounts' pages are formatted differently. Once it worked for my photo streams, I stopped working on it :)
+
+# Disclaimer
+
+Use at your own risk. I take no responsibility for how you use this script. This script was honestly designed to help me, a Flickr user whose copywrighted photos are stuck on Flickr servers, and maybe other folks in my situation. If by some miracle these folks come across this project when googling for solutions. Please don't use it to steal others' photos without permission.
+
+# Author
+    Jonathan Foote
+    jmfoote@loyola.edu
+    18 Feb 2014
---
+++ flickrgrabr.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import re, urllib2, sys, os, time
+
+def dump_err_page(errdir, uri, page):
+    '''
+    If errdir is set, dumps page to disk file with name based on uri at errdir.
+    Otherwise does nothing.
+    '''
+    if not errdir:
+        return
+    if not os.path.exists(errdir):
+        os.makedirs(errdir)
+    filename = [j for j in uri.split("/") if j][-1]+".html"
+    filepath = os.path.join(errdir, filename)
+    sys.stderr.write("dumping page to %s\n" % filepath)
+    if os.path.exists(filepath):
+        sys.stderr.write("warning: overwriting page at %s" % filepath)
+    file(filepath, "wt").write(page)
+
+def parse_photostream(uri, outdir=".", errdir="", sleep_sec=3):
+    '''
+    Parses Flickr photostream at uri. Downloads original image from link on page
+    a uri to outdir. If errdir is set and parsing errors are detected, pages 
+    triggering problems are downloaded to errdir. Returns a tuple of the 
+    original image URI and the URI of the next page in the photostream.
+    '''
+
+    # download page
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
+    page = urllib2.urlopen(urllib2.Request(uri, None, headers)).read()
+
+    # get original image URI
+    img_uri = None
+    regex = 'img src=&quot;(http(?:s)?\:\/\/.*_o?\.jpg)&quot'
+    mobj = re.search(regex, page)
+    if mobj:
+        img_uri = mobj.groups()[0]
+    else:
+        sys.stderr.write("couldn't find original image uri for %s\n" % uri)
+        dump_err_page(errdir, uri, page)
+
+    # download image
+    filename = os.path.join(outdir, img_uri.split("/")[-1])
+    img = urllib2.urlopen(urllib2.Request(img_uri, None, headers)).read()
+    file(filename, "wb").write(img)
+    
+    # get next page uri
+    next_uri = None
+    regex = '\<a id="nav-bar-next" data-track="next_button" class="Butt ' \
+            'visible rapidnofollow" href="\/photos\/(.*)\/in\/photostream\/" ' \
+            'tabindex="3"\>'
+    mobj = re.search(regex, page)
+    if mobj:
+        keystr = mobj.groups()[0]
+        next_uri = "http://www.flickr.com/photos/%s/in/photostream/" % keystr
+    else:
+        sys.stderr.write("couldn't find next page uri for %s\n" % uri)
+        dump_err_page(errdir, uri, page)
+
+    return img_uri, next_uri
+    
+def scrape(start, outdir, errdir, sleep_sec):
+    '''
+    Scrapes images from photostream with first page at start to outdir. 
+    If errdir is set, pages that trigger parsing errors are downloaded to errdir
+    '''
+    next_uri = start
+    while(next_uri):
+        img_uri, next_uri = parse_photostream(next_uri, outdir, errdir, 
+            sleep_sec)
+        print "processed", next_uri, img_uri
+        if sleep_sec:
+            time.sleep(sleep_sec)
+        
+if __name__ == "__main__":
+    import argparse
+    desc = "Downloads your original photos from your Flickr photostream. " \
+           "NOTE: This is a quick hack I used to get some of my photos back. "\
+           "It currently doesn't work with all streams, " \
+           "and will probably work with less as time goes on :|"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-o,--outdir', metavar="OUTDIR", dest="outdir", 
+        default=".", help="Specify directory to download images to.")
+    parser.add_argument('-e,--errdir', metavar="ERRDIR", dest="errdir", 
+        default="", help="Specify a directory to output pages that fail to " \
+        "parse correctly to.")
+    parser.add_argument('-s,--sleep-time-sec', metavar="SLEEPTIME", 
+        dest="sleep_sec", default="3", help="Number of seconds to wait " \
+        "between getting pages from flickr.")
+    parser.add_argument('STARTURI', help="URI of first page of photostream, " \
+        "something like this: " \
+        "http://www.flickr.com/photos/homatthew/12267802976")
+    args = parser.parse_args()
+    
+    scrape(args.STARTURI, args.outdir, args.errdir, int(args.sleep_sec))
